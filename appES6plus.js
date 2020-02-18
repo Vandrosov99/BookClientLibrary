721c945b479de249157cf147b1136620b33b8858
Vandrosov <rpk633@gmail.com>
2020-02-18 19:34:59 +0300

    refactoring comments

--- appES6plus.js
+++ appES6plus.js
@@ -70,68 +70,7 @@ class UI {
     }
 }
 
-// UI.prototype.addBookToList = function (book) {
 
-//     //берем наш елемент с tbody
-//     const list = document.getElementById('bookList');
-
-//     //создаем tr элемент для tbody части таблицы
-//     const row = document.createElement('tr');
-
-//     //вставляем колонки
-//     row.innerHTML = `<td>${book.title}</td>
-//                     <td>${book.author}</td>
-//                     <td>${book.isbn}</td>
-//                     <td><a href="#" class="delete">X</a></td>`
-
-//     //вставляем в tbody конструкицю tr ---> td
-//     list.appendChild(row)
-
-// }
-// UI.prototype.clearFields = function () {
-//     document.getElementById('title-id').value = "";
-//     document.getElementById('author-id').value = "";
-//     document.getElementById('code-id').value = "";
-// }
-// UI.prototype.showAlert = function (message, className, book) {
-//     test = (book !== undefined) ? book : 'lol';
-//     //название книги
-//     // bookName = book.title;
-//     //создаем div
-//     div = document.createElement('div');
-//     //добавили класс
-//     div.className = `alert ${className}`;
-//     //в див вставлям message
-//     if (book === undefined) {
-//         book = '';
-//         div.appendChild(document.createTextNode(message));
-//     } else {
-//         div.appendChild(document.createTextNode(message + test.title));
-//     }
-
-
-
-//     //что бы вставить div мы должны знать что идет перед ним и что идет после него
-
-//     const container = document.querySelector('.container');
-//     const form = document.querySelector('form');
-//     container.insertBefore(div, form);
-
-
-//     //пропадание div с success + error
-//     setTimeout(function () {
-//         document.querySelector('.alert').remove();
-//     }, 3000)
-
-// }
-// UI.prototype.deleteBook = function (target) {
-
-//     if (target.className === 'delete') {
-//         target.parentElement.parentElement.remove();
-//         console.log(target);
-//     }
-
-// }
 //все события (events)
 function allEventRun() {
     //добавление книг 
